01b0ebf5a78fa8d8eced014cb318b63a68e7431a
Chae Yeon Lee <130931438+chaeyeonlee07@users.noreply.github.com>
2023-05-23 15:29:58 +0900

    Longest increasing substring

--- bj/14002.py
+++ bj/14002.py
@@ -1,12 +1,24 @@
-from cmath import inf
-import sys
-n = int(sys.stdin.readline())
-dp = [1] * n
-a = list(map(int, sys.stdin.readline().split()))
-
-for i in range(len(a)):
+import sys 
+n = int(sys.stdin.readline()) 
+numbers = list(map(int, sys.stdin.readline().split()))
+dp = [0 for _ in range(n)]
+trace = [[] for _ in range(n)]
+maximum = 0 
+for i in range(n):
     for j in range(i):
-        if a[j] < a[i]:
-            dp[i] = max(dp[i], dp[j] + 1 )
-            
-     
+        if numbers[j] < numbers[i]: 
+            if dp[j] > dp[i] :
+                dp[i] = dp[j]
+                maximum = j
+    if maximum < n and maximum != i: 
+        for elem in trace[maximum]:
+            trace[i].append(elem)
+        
+    dp[i] += 1 
+    maximum = i + 1 
+    trace[i].append(numbers[i]) 
+print(max(dp))
+for i in range(n):
+    if len(trace[i]) == max(dp):
+        print(*trace[i]) 
+        break
